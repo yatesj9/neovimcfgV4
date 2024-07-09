05b586bf536d0c9a3c7703d1ed951130dd8cc6c8
Jason Yates <yatesj9@gmail.com>
2024-07-09 09:19:40 -0400

    CPP build settings, and mappings

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -35,7 +35,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "python",
+        -- "python",
         -- add more arguments for adding more debuggers
       })
     end,
